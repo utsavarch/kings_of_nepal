af805f03e4a1a7290493c023dd3ea32c539fa6e2
Utsav Acharya Sharma <71218506+utsavarch@users.noreply.github.com>
2024-08-27 09:01:42 +0545

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="h-[70vh] w-[100vw] bg-[url('./img/hero.png')] bg-top bg-cover flex flex-col items-center justify-center text-center">
       <div className="sm:h-[200px] sm:w-[500px] h-[150px] w-[250px] flex items-center justify-center mb-4">
         <img
-          src="src/img/map.png"
+          src="https://github.com/utsavarch/kings_of_nepal/blob/main/src/img/hero.png?raw=true"
           className="h-full w-full object-contain"
         ></img>
       </div>
