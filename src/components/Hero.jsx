bc5fa6b86590dfa3d69fa11f099ddafe6f78667d
Utsav Acharya Sharma <71218506+utsavarch@users.noreply.github.com>
2024-08-27 16:13:00 +0545

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="h-[70vh] w-[100vw] bg-[url('./img/hero.png')] bg-top bg-cover flex flex-col items-center justify-center text-center">
+    <div className="h-[70vh] w-[full] bg-[url('./img/hero.png')] bg-top bg-cover flex flex-col items-center justify-center text-center">
       <div className="sm:h-[200px] sm:w-[500px] h-[150px] w-[250px] flex items-center justify-center mb-4">
         <img
           src="https://github.com/utsavarch/kings_of_nepal/blob/main/src/img/map.png?raw=true"
