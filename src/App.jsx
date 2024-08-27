35b1123bf8ac1d5d3a868b21117be7f1f96c1da9
Utsav Acharya Sharma <71218506+utsavarch@users.noreply.github.com>
2024-08-27 15:12:11 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
       <Hero />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {kings.map((king, index) => (
           <Card key={index} king={king} onClick={() => handleCardClick(king)} />
         ))}
