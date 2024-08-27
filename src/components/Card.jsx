a7067ae28a95e1f6b79890f05ac60d44267f5265
Utsav Acharya Sharma <71218506+utsavarch@users.noreply.github.com>
2024-08-27 15:28:11 +0545

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ king, onClick }) {
   return (
     <div
-      className="bg-[#f8fafc] m-5 p-5 rounded-[30px] hover:scale-[1.02] shadow-lg shadow-black hover:cursor-pointer"
+      className="bg-[#f8fafc] m-5 p-5 rounded-[30px] hover:scale-[1.02] shadow-lg shadow-black hover:cursor-pointer flex flex-col justify-center"
       onClick={onClick}
     >
       <img src={king.img} alt={king.name} className="rounded-[100%]" />
